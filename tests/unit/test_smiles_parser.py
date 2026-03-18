527c5fc4cffa60af30684d96dabdb3e16bc62cbf
IshikaRaj1311 <pes2ug23cs923@pesu.pes.edu>
2026-03-18 12:00:39 +0530

    Add SMILES parser's unit tests using RDKit

---
+++ tests/unit/test_smiles_parser.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import pytest
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+
+import pytest
+from molecular_processing.smiles_parser import validate_smiles
+
+
+def test_valid_smiles():
+
+    mol = validate_smiles("CCO")
+    assert mol is not None
+
+
+def test_invalid_smiles():
+
+    with pytest.raises(Exception):
+        validate_smiles("INVALID_SMILES")
